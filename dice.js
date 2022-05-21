65e1d42d3714779d6cfe52a553ba01c41a2098a5
moreindira <99152158+moreindira@users.noreply.github.com>
2022-05-21 17:00:25 +0530

    Add files via upload

---
+++ dice.js
@@ -0,0 +1,33 @@
+var dice;
+// dice image from value 1 to 6 are given in an array, codes are from Die face Emoji Emojipedia
+var dices = ['&#9856;', '&#9857;', '&#9858;', '&#9859;', '&#9860;', '&#9861;']; 
+var stopped = true;
+ var t;
+// for keep on changing the dice emoj is
+function change()
+{ 
+    var random = Math.floor(Math.random() * 6);
+     dice.innerHTML = dices[random]; 
+} 
+function stopstart()
+ {
+    if (stopped) 
+    {
+        stopped = false;
+        //change function is called here to change theimage for every 0.1 seconds
+        t = setInterval(change, 100);
+    }
+     else 
+     {
+        clearInterval(t);
+        stopped = true;
+    }
+}
+// invoking function
+window.onload =function() 
+{
+    dice = document.getElementById("dice");
+    stopstart();
+}
+
+
